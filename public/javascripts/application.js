794c4f2b711a380da29286d16e6c84c0eec8171b
Collin Glass <collinglass@gmx.com>
2013-06-07 19:51:52 -0400

    Initial Commit

---
+++ public/javascripts/application.js
@@ -0,0 +1,21 @@
+window.App = Ember.Application.create();
+
+DS.Store.create({
+  revision: 12,
+  adapter: DS.RESTAdapter.create({
+    namespace: 'api'
+  })
+});
+
+
+App.Kitten = DS.Model.extend({
+  name: DS.attr('string'),
+  picture: DS.attr('string')
+});
+
+App.IndexRoute = Ember.Route.extend({
+  model: function() {
+    return App.Kitten.find();
+  }
+});
+
